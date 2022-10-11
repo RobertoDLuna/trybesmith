c87a93a48091c96b08f60ecfe0cfabc74708d72c
Roberto Luna <robertocgw@gmail.com>
2022-10-11 20:11:38 -0300

    feat: req01 - Crie um endpoint para o cadastro de produtos

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-// começando
-
 import express from 'express';
+import productRouter from './routes/product.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productRouter);
 
 export default app;
